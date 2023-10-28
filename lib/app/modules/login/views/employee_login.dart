a5d792c148980ea0f66f4b248a668afb2a9ab00c
anubiju22 <anuit16@gmail.com>
2023-10-28 11:26:05 +0530

    main

--- lib/app/modules/login/views/employee_login.dart
+++ lib/app/modules/login/views/employee_login.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:kvn_farm_rich/app/common_widgets/button/loginbutton.dart';
 import 'package:kvn_farm_rich/app/common_widgets/svg_icons/svg_widget.dart';
 import 'package:kvn_farm_rich/app/common_widgets/textfield/logintextfeild.dart';
 import 'package:kvn_farm_rich/app/common_widgets/texts/login_text.dart';
 import 'package:kvn_farm_rich/constraints/app_colors.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import '../controllers/login_controller.dart';
 
